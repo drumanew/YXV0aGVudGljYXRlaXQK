f52fc461ca79d307ab88e84c56b0706006670808
Mikhail Khotko <mkhotko@debian>
2016-10-03 17:21:25 +0500

    Add param maximum number of workers in seq_calc

--- seq_calc/src/seq_calc_app.erl
+++ seq_calc/src/seq_calc_app.erl
@@ -4,8 +4,14 @@
 -export([start/2]).
 -export([stop/1]).
 
+-define (DEFAULT_NUMBER_OF_WORKERS, 500).
+
 start(_Type, _Args) ->
-	seq_calc_sup:start_link().
+  case application:get_env(seq_calc, maximum_number_of_workers) of
+    {ok, N} -> seq_calc_sup:start_link(N);
+    _       -> seq_calc_sup:start_link(?DEFAULT_NUMBER_OF_WORKERS)
+  end.
+
 
 stop(_State) ->
 	ok.
--- seq_calc/src/seq_calc_sup.erl
+++ seq_calc/src/seq_calc_sup.erl
@@ -1,12 +1,12 @@
 -module(seq_calc_sup).
 -behaviour(supervisor).
 
--export([start_link/0]).
+-export([start_link/1]).
 -export([init/1]).
 
-start_link() ->
-	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+start_link(MaxWorkers) ->
+	supervisor:start_link({local, ?MODULE}, ?MODULE, MaxWorkers, []).
 
-init([]) ->
+init(_MaxWorkers) ->
 	Procs = [],
 	{ok, {{one_for_one, 1, 5}, Procs}}.
